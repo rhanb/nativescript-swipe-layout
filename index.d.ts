83327aa4846d57069a5ba2432bd2e9cfa7aa2674
Nathan Walker <walkerrunpdx@gmail.com>
2017-02-08 17:55:49 -0800

    chore: cleanup index.d.ts for a better start

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,6 @@
-/**
- * iOS and Android apis should match.
- * It doesn't matter if you export `.ios` or `.android`, either one but only one.
- */
-export * from './yourplugin.ios';
-
-// Export any shared classes, constants, etc.
-export * from './yourplugin.common';
+import { Common } from './yourplugin.common';
+export declare class YourPlugin extends Common {
+  // define your typings manually
+  // or..
+  // use take the ios or android .d.ts files and copy/paste them here
+}
