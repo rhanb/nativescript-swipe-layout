46bc15cd55a417ba170088e67776eefd691da197
PROGRESS\radeva <veselina.radeva@telerik.com>
2017-03-07 17:00:04 +0200

    Remove platform on npm run test.android / npm run test.ios

--- yourplugin.common.d.ts
+++ yourplugin.common.d.ts
@@ -2,6 +2,7 @@ import { Observable } from 'data/observable';
 export declare class Common extends Observable {
     message: string;
     constructor();
+    greet(): string;
 }
 export declare class Utils {
     static SUCCESS_MSG(): string;
