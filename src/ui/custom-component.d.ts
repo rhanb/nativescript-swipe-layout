37556de272391bdb0e12577d1a28fdb17ce8c343
Dimitar Tachev <Dimitar.Tachev@progress.com>
2017-06-01 11:07:45 +0300

    Removed the plugin UI folder from the seed.

--- src/ui/custom-component.d.ts
+++
@@ -1,6 +0,0 @@
-import { GridLayout } from 'ui/layouts/grid-layout';
-import { Property } from 'ui/core/view';
-export declare class CustomComponent extends GridLayout {
-    constructor();
-}
-export declare const messageProperty: Property<CustomComponent, string>;
