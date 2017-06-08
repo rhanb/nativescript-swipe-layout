95f220c5da87644ff918e1f80cc853c2ff627c76
Dimitar Tachev <Dimitar.Tachev@progress.com>
2017-06-08 18:54:19 +0300

    Fixed the tns-core-modules paths.

--- demo/app/main-view-model.ts
+++ demo/app/main-view-model.ts
@@ -1,4 +1,4 @@
-import { Observable } from 'data/observable';
+import { Observable } from 'tns-core-modules/data/observable';
 import { YourPlugin } from 'nativescript-yourplugin';
 
 export class HelloWorldModel extends Observable {
--- demo/app/tests/tests.js
+++ demo/app/tests/tests.js
@@ -1,13 +1,12 @@
 var YourPlugin = require("nativescript-yourplugin").YourPlugin;
 var yourPlugin = new YourPlugin();
 
-// TODO replace 'functionname' with an acual function name of your plugin class and run with 'npm test <platform>'
 describe("greet function", function() {
-  it("exists", function() {
-    expect(yourPlugin.greet).toBeDefined();
-  });
+    it("exists", function() {
+        expect(yourPlugin.greet).toBeDefined();
+    });
 
-  it("returns a string", function() {
-    expect(yourPlugin.greet()).toEqual("Hello, NS");
-  });
+    it("returns a string", function() {
+        expect(yourPlugin.greet()).toEqual("Hello, NS");
+    });
 });
